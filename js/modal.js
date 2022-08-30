fbd35bb41eeabaa4208b7b2d585e3a1b4763af0f
Michał Murawski <109108678+AMichalMurawski@users.noreply.github.com>
2022-08-30 09:28:17 +0200

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -2,6 +2,7 @@
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
+    closeModalBtn: document.querySelector('body'),
     modal: document.querySelector('[data-modal]'),
   };
 
